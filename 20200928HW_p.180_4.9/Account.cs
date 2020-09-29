fce02abc0962e62b947bfdd699ea370523f9bd18
NoZ-design <68496572+NoZ-design@users.noreply.github.com>
2020-09-29 16:55:31 +0800

    20200928HW_p.180_4.9

---
+++ 20200928HW_p.180_4.9/Account.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+
+namespace _20200928HW_p._180_4._9
+{
+    class Account
+    {
+        public string Name { get; set; }
+        private decimal balance;
+
+        public Account(string accountName,decimal initialBalance)
+        {
+            Name = accountName;
+            balance = initialBalance;
+        }
+
+        public decimal Balance
+        {
+            get
+            {
+                return balance;
+
+            }
+            private set
+            {
+                if(value > 0.0m)
+                {
+                    balance = value;
+                }
+            }
+        }
+
+        public void Deposit(decimal depositAmount)
+        {
+            if(depositAmount > 0.0m)
+            {
+                Balance = Balance + depositAmount;
+            }
+        }
+
+        public int Withdrawal(decimal WithdrawalAmount)
+        {
+            if(WithdrawalAmount > 0.0m)
+            {
+                Balance = Balance - WithdrawalAmount;
+                if(Balance < 0.0m)
+                {
+                    Console.WriteLine($"Withdrawing {WithdrawalAmount:C} would leave you overdrawn!");
+                }
+            
+            }
+            return (int)Balance;
+        }
+    }
+}
---
+++ 20200928HW_p.180_4.9/AccountTest.cs
@@ -0,0 +1,52 @@
+﻿using System;
+
+namespace _20200928HW_p._180_4._9
+{
+    class AccountTest
+    {
+        static void Main(string[] args)
+        {
+            Account account1 = new Account("Jane", 00.00m);
+            Account account2 = new Account("Jhon", 00.00m);
+
+            Console.WriteLine($"{account1.Name}'s balance:{account1.Balance:C}");
+            Console.WriteLine($"{account2.Name}'s balance:{account2.Balance:C}");
+
+            //account1存錢
+            Console.Write("\nEnter deposite amount for account1: ");
+            decimal depositAmount = decimal.Parse(Console.ReadLine());
+            Console.WriteLine($"adding {depositAmount:C} to account1 balance\n");
+            account1.Deposit(depositAmount);
+
+            Console.WriteLine($"{account1.Name}'s balance:{account1.Balance:C}");
+            Console.WriteLine($"{account2.Name}'s balance:{account2.Balance:C}");
+
+            //account2存錢
+            Console.Write("\nEnter deposite amount for account2: ");
+            depositAmount = decimal.Parse(Console.ReadLine());
+            Console.WriteLine($"adding {depositAmount:C} to account2 balance\n");
+            account2.Deposit(depositAmount);
+
+            Console.WriteLine($"{account1.Name}'s balance:{account1.Balance:C}");
+            Console.WriteLine($"{account2.Name}'s balance:{account2.Balance:C}");
+
+            //account1提款
+            Console.Write("\nEnter withdrawal amount for account1: ");
+            decimal WithdrawalAmount = decimal.Parse(Console.ReadLine());
+            Console.WriteLine($"removing {WithdrawalAmount:C} to account1 balance\n");
+            account1.Withdrawal(WithdrawalAmount);
+
+            Console.WriteLine($"{account1.Name}'s balance:{account1.Balance:C}");
+            Console.WriteLine($"{account2.Name}'s balance:{account2.Balance:C}");
+
+            //account2提款
+            Console.Write("\nEnter withdrawal amount for account2: ");
+            WithdrawalAmount = decimal.Parse(Console.ReadLine());
+            Console.WriteLine($"removing {WithdrawalAmount:C} to account2 balance\n");
+            account2.Withdrawal(WithdrawalAmount);
+
+            Console.WriteLine($"{account1.Name}'s balance:{account1.Balance:C}");
+            Console.WriteLine($"{account2.Name}'s balance:{account2.Balance:C}");
+        }
+    }
+}
